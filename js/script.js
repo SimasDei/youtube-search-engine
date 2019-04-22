2bd3f3793a639b33a760afed280b580527cee057
SimasDei <simondeiman@gmail.com>
2019-04-22 20:46:44 +0300

    add fetch function to get google results based on query

--- js/script.js
+++ js/script.js
@@ -37,4 +37,40 @@ $(document).ready(function() {
       );
     }
   });
+
+  $('#search-form').submit(function(e) {
+    e.preventDefault();
+  });
 });
+
+const API = {
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  key: 'AIzaSyDolRhAuSs-3GcB0TZQv1tmqZ_pFTr6Pzk'
+};
+
+function search() {
+  $('#results').html('');
+  $('#buttons').html('');
+
+  var q = $('#query').val();
+
+  $.get(
+    `${API.url}`,
+    {
+      part: 'snippet, id',
+      q,
+      type: 'video',
+      key: API.key
+    },
+    function(data) {
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      $.each(data.items, function(i, item) {
+        var output = getOutput(item);
+
+        $('#results').append(output);
+      });
+    }
+  );
+}
